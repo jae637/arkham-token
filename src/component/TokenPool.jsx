0688b61aebd5f7cc538ddb63df5b11365d9ae689
Karl <jae637@naver.com>
2023-06-25 22:33:43 +0900

    [UPDATE] 애니메이션 효과 적용

--- src/component/TokenPool.jsx
+++ src/component/TokenPool.jsx
@@ -7,7 +7,7 @@ import './Token.css'
 function TokenPool({ tokenStringList, setTokenList }) {
     const animationSetup = {
         from: { opacity: 0 },
-        to: { opacity: 1 }
+        to: { opacity: 1 },
     }
     const [init, setInit] = useState(false);
     const [tokenPool, setTokenPool] = useState([]);
@@ -26,11 +26,9 @@ function TokenPool({ tokenStringList, setTokenList }) {
     // 토큰 리스트를 랜더링 할때 사용
     const TokenList = useCallback(() =>
         tokenPool.map((item, idx) => <Col md={2} sm={3} xs={4} className="my-4 d-flex justify-content-center">
-            <animated.div style={{ ...springs }}>
-                <Token src={item} init={init} setInit={setInit} key={idx} />
-            </animated.div>
+            <Token src={item} init={init} setInit={setInit} key={idx} />
         </Col>)
-        , [springs, tokenPool, init])
+        , [tokenPool, init])
 
     useEffect(() => {
         shuffleToken(tokenStringList);
@@ -39,9 +37,9 @@ function TokenPool({ tokenStringList, setTokenList }) {
     return (
         <Container style={{ minHeight: window.innerHeight }} className="d-flex webfont-text">
             <div style={{ flexWrap: 'wrap' }} className="d-flex align-content-center justify-content-center">
-                <Row>
+                <animated.div className="row" style={{ ...springs }}>
                     <TokenList />
-                </Row>
+                </animated.div>
                 <Row className="m-2 mt-5 w-75">
                     <Button size="lg" className="my-2 mixtoken" onClick={() => { setInit(true); api.start(animationSetup) }} >토 큰 섞 기</Button>
                     <Button size="lg" variant="warning" className="mt-2 moditoken" onClick={() => { setTokenInit() }}>뒤 로 가 기</Button>
